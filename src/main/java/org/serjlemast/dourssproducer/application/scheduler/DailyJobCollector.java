034476ad2ab8fa050b70f105487c9c9f543ece8b
Dimitrii Maksymov <dimitriimaksymov@Dimitriis-MacBook-Air.local>
2026-02-03 23:30:15 +0200

    add log

--- src/main/java/org/serjlemast/dourssproducer/application/scheduler/DailyJobCollector.java
+++ src/main/java/org/serjlemast/dourssproducer/application/scheduler/DailyJobCollector.java
@@ -1,10 +1,12 @@
 package org.serjlemast.dourssproducer.application.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.serjlemast.dourssproducer.application.usecase.CollectJobsUseCase;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DailyJobCollector {
@@ -13,6 +15,7 @@ public class DailyJobCollector {
 
     @Scheduled(cron = "${cron.job}")
     public void run() {
+        log.info("start scheduling processing jobs");
         useCase.collect();
     }
 }
